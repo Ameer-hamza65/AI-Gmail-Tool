87b242f77cee281c087f413880881afa41447866
Ameer-hamza65 <22-se-38@students.uettaxila.edu.pk>
2025-08-12 15:54:44 +0500

    a

---
+++ agent.py
@@ -0,0 +1,22 @@
+from tool import tools
+import os
+from langchain_groq import ChatGroq
+from langgraph.prebuilt import create_react_agent
+
+
+api_key=os.getenv('GROQ_API_KEY')
+
+llm=ChatGroq(
+    model='llama-3.3-70b-versatile',
+    api_key=api_key
+)
+
+graph=create_react_agent(llm,tools)
+example_query = "Draft an email to hamza5993795@gmail.com thanking them for coffee."
+
+events = graph.stream(
+    {"messages": [("user", example_query)]},
+    stream_mode="values",
+)
+for event in events:
+    event["messages"][-1].pretty_print()
